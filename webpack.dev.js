9fdb9e1f7ae129ffe87483f683ed4f31abb1cae6
Tyler Lentz <tyler.cg.lentz@gmail.com>
2019-12-16 12:41:35 -0600

    chrome extension reloads

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,12 +3,14 @@
 const webpack = require("webpack");
 const WebpackMerge = require("webpack-merge");
 const commonConfig = require("./webpack.common.js");
+const ChromeExtensionReloader = require("webpack-chrome-extension-reloader");
 
 module.exports = WebpackMerge(commonConfig, {
   devtool: "inline-source-map",
   plugins: [
     new webpack.NamedModulesPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin(),
+    new ChromeExtensionReloader()
   ],
   module: {
     rules: [
