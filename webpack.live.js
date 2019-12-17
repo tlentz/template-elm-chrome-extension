7ccc58a2289f42e77e129c6ff79b1d86308683fe
Tyler Lentz <tyler.cg.lentz@gmail.com>
2019-12-17 13:08:59 -0600

    change live webpack config

--- webpack.live.js
+++ webpack.live.js
@@ -2,11 +2,35 @@
 
 const outputDir = "public";
 
+const webpack = require("webpack");
 const WebpackMerge = require("webpack-merge");
+const commonConfig = require("./webpack.common.js");
 
-const devConfig = require("./webpack.dev.js");
-
-module.exports = WebpackMerge(devConfig, {
+module.exports = WebpackMerge(commonConfig, {
+  devtool: "inline-source-map",
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: [
+          { loader: "elm-hot-webpack-loader" },
+          {
+            loader: "elm-webpack-loader",
+            options: {
+              // add Elm's debug overlay to output
+              debug: false,
+              forceWatch: true
+            }
+          }
+        ]
+      }
+    ]
+  },
   devServer: {
     contentBase: `./${outputDir}`,
     port: 9000,
